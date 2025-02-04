c6f8e5df17b109ed6636b62db7b1f80a6e4098fe
Ishan Thathsara Weerakoon <31079429+ishancoderr@users.noreply.github.com>
2025-02-04 01:02:28 +0100

    add initial react app

---
+++ chat-app/src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='sidebar'>
+        <div className='upperSlide'>
+          <div className='upperSlideTop'>
+          <button className="mitBtn"><img src="" alt="" className="addBtn"/>New Chat</button>
+          <div className="upperSideBottom">
+            <button className="query"><img src="" alt=""/>What is Programming ?</button>
+            <button className="query"><img src="" alt=""/>What is Programming ?</button>
+          </div>
+          </div> 
+        </div> 
+      </div>
+      <div className='main'>
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
